a850663ab7f2527f532e13ff0e99256dcbbb0291
Fatitboo <21522448@gm.uit.edu.vn>
2024-05-06 21:12:37 +0700

    add backend with db postgres db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { MyLoggerModule } from './my-logger/my-logger.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST,
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      ssl: true,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ThrottlerModule.forRoot([{
+      name: 'short',
+      ttl: 100,
+      limit: 5,
+    },
+    {
+      name: 'long',
+      ttl: 60000,
+      limit: 100,
+    }]),
+    MyLoggerModule,
+    UserModule
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
